bb7117ba8e3b9efd982e4d31e6236483b1bada33
Pascal Ahmadu <pahmadu@mercycorps.org>
2025-04-06 09:42:22 +0100

    fixed jobs

--- src/pages/Companies.tsx
+++ src/pages/Companies.tsx
@@ -169,7 +169,7 @@ export default function Companies() {
                     Company Profile
                   </button>
                   <button
-                    onClick={() => navigate(`/companies/${company.id}/jobs`)}
+                    
                     className="bg-white text-indigo-600 px-4 py-2 border border-indigo-600 hover:bg-indigo-50 transition-colors flex items-center"
                   >
                     View Jobs
