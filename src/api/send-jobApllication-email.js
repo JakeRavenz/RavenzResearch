b274e282d98951deae302339afa1e68973333356
UgbabeOG <ugbabe.og@gmail.com>
2025-04-15 17:52:28 +0100

    Update email subject and body for job application confirmation

--- src/api/send-jobApllication-email.js
+++ src/api/send-jobApllication-email.js
@@ -45,16 +45,20 @@ export default async function handler(req, res) {
     const mailOptions = {
       from: `"Ravenz Research" <${process.env.EMAIL_USER}>`,
       to: email,
-      subject: `Application Successful: ${jobTitle}`,
+      subject: `Application Recieved for ${jobTitle} - Ravenz Research`,
       html: `
         <div style="font-family: Arial, sans-serif; max-width: 600px; margin: 0 auto; padding: 20px; border: 1px solid #ddd; border-radius: 5px;">
           <h2 style="color: #3a86ff; text-align: center;">Congratulations on Your Application!</h2>
           <p>Dear <strong>${firstName}</strong>,</p>
-          <p>Thank you for applying to <strong>${jobTitle}</strong> for the position of <strong>${jobPosition}</strong> at Ravenz Research. We are thrilled to have you as a candidate!</p>
+          <p>We’ve successfully received your application for the <strong>${jobTitle}</strong> for the role of <strong>${jobPosition}</strong> at Ravenz Research. We are thrilled to have you as a candidate!</p> 
+          <p>Our team will review your submission shortly, and if your profile is shortlisted, you’ll receive further instructions regarding the next steps, including identity verification and onboarding.</p>
+          <p>In the meantime, please ensure your contact details remain active and check your email regularly for updates.
+          </p>
+          <p>We appreciate your interest in joining our team and look forward to the possibility of working together.</p>
           <p>You can view the job details and track your application status using the link below:</p>
            <p><a href="${jobLink}" style="color: #3a86ff; text-decoration: none;">View Job Details</a></p>
           <p>If you have any questions, feel free to reply to this email.</p>
-          <p>Best regards,<br>The Ravenz Research Team</p>
+          <p>Best regards,<br><strong>Ravenz Research</strong><br>www.RavenzResearch.com</p>
         </div>
       `,
       replyTo: process.env.REPLY_EMAIL || process.env.EMAIL_USER,
